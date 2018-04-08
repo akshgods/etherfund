39538e4a46dbacfb475cfcc008eae132d191bcce
Lichen <LC@Lichens-MacBook-Air.local>
2018-04-08 11:28:48 -0500

    added verification for token expiry for client side

--- src/app/components/UserProfile.js
+++ src/app/components/UserProfile.js
@@ -4,14 +4,20 @@ import LoginSignup from "../user/LoginSignup";
 import { connect } from "react-redux";
 import { Modal, Button, Dropdown } from "semantic-ui-react";
 import { logout } from "../user/userActionCreator";
+import jwt from "jsonwebtoken";
+import config from "../../utils/config";
 
 const mapStateToProps = state => ({
   isLogin: state.auth.isLogin,
+  token: state.auth.token,
   user: state.auth.userInfo
 });
 
 const mapDispatchToProps = dispatch => ({
   onLogoutClick() {
+    dispatch(logout());
+  },
+  onUserVerifyFailure() {
     dispatch(logout());
   }
 });
@@ -21,48 +27,95 @@ class UserProfile extends React.Component {
     this.props.onLogoutClick();
   };
 
+  componentDidMount = () => {
+    if (this.props.isLogin) {
+      jwt.verify(this.props.token, config.jwtSecret, (err, decoded) => {
+        if (err) {
+          console.error(err)
+          this.props.onUserVerifyFailure();
+        }
+        const date = decoded.expiry;
+
+        if (date !== new Date().getMonth()) {
+          this.props.onUserVerifyFailure();
+        }
+      });
+    }
+  };
+
   render() {
     const { fixed, isLogin } = this.props;
 
     switch (isLogin) {
       case true:
         const { firstName, lastName } = this.props.user;
-        return <div className="down">
+        return (
+          <div className="down">
             <Dropdown text={"Hello, " + firstName}>
               <Dropdown.Menu>
                 <Dropdown.Item disabled>
                   <span>
-                    Signed in as <strong>
+                    Signed in as{" "}
+                    <strong>
                       {firstName} {lastName}
                     </strong>
                   </span>
                 </Dropdown.Item>
                 <Link className="item" to="/explore">
-                  {"Your Profile"}
+                  {"My Campaigns"}
                 </Link>
                 <Link className="item" to="/explore">
-                  {"Your Wallet"}
+                  {"My Contributions"}
                 </Link>
+                <Link className="item" to="/explore">
+                  {"My Wallet"}
+                </Link>
+                <Link className="item" to="/explore">
+                  {"Profile"}
+                </Link>
+                <Dropdown.Divider />
                 <Dropdown.Item onClick={this.handleClick}>
                   {"Sign Out"}
                 </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-          </div>;
+          </div>
+        );
 
       default:
-        return <div>
-            <Modal size="tiny" trigger={<Button size="medium" compact className={!fixed ? "inverted" : "google plus"}>
+        return (
+          <div>
+            <Modal
+              size="tiny"
+              trigger={
+                <Button
+                  size="medium"
+                  compact
+                  className={!fixed ? "inverted" : "google plus"}
+                >
                   Log In
-                </Button>}>
+                </Button>
+              }
+            >
               <LoginSignup isLogin={true} />
             </Modal>
-            <Modal size="tiny" trigger={<Button size="medium" compact className={!fixed ? "inverted" : "primary"} style={{ marginLeft: "0.5em" }}>
+            <Modal
+              size="tiny"
+              trigger={
+                <Button
+                  size="medium"
+                  compact
+                  className={!fixed ? "inverted" : "primary"}
+                  style={{ marginLeft: "0.5em" }}
+                >
                   Sign Up
-                </Button>}>
+                </Button>
+              }
+            >
               <LoginSignup isLogin={false} />
             </Modal>
-          </div>;
+          </div>
+        );
     }
   }
 }
