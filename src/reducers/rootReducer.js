42f500c3b4082825e33c440045756be9ad37c4d5
unknown <Cli@ENG-CHEN-LP.domain.local>
2018-04-11 16:13:23 -0500

    working on web3 dev

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -6,6 +6,7 @@ import exploreReducer from "../app/explore/exploreReducer";
 import formReducer from "../app/campaign/formReducer";
 import userReducer from "../app/user/userReducer";
 import authReducer from "../app/user/authReducer";
+import web3Reducer from "../utils/web3/Web3Reducer";
 
 const rootReducer = combineReducers({
   test: testReducer,
@@ -13,6 +14,7 @@ const rootReducer = combineReducers({
   form: formReducer,
   router: routerReducer,
   user: userReducer,
+  web3: web3Reducer,
   auth: authReducer
 });
 
